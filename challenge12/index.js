579ca4a2950b1dbd47b34544b24a9062a276a0e4
Mariana  Cruz <mda-cruz@Marianas-Air-2.Home>
2023-04-04 10:57:57 +0100

    fix

--- challenge12/index.js
+++
@@ -1,9 +0,0 @@
-const theMenu = document.querySelector(".menu");
-const menuBtn = document.querySelector("#menu-button");
-
-console.log(theMenu);
-console.log(menuBtn);
-
-menuBtn.addEventListener("click", () => {
-  theMenu.classList.toggle("hidden");
-});
